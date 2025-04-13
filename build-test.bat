0e7a9a72db2a0a428bfe427a50b0205580d32810
Tim Whiting <tim@whitings.org>
2025-04-12 23:17:59 -0600

    update

--- build-test.bat
+++ build-test.bat
@@ -1,13 +1,15 @@
 :: Build the kokac tool
-koka.exe -obin/kokac.exe bin/kokac.kk
+koka.exe -o"bin/kokac.exe" "bin/kokac.kk"
 :: Make it executable
-attrib +x bin/kokac.exe
+attrib +x bin\kokac.exe
 
 :: Remove old versions of the test project
-rmdir /r /f test2
+if exist test2 (
+  rd /S /Q test2 
+)
 :: Init a test project
-./bin/kokac.exe init test2
+.\bin\kokac.exe init test2
 :: Change to the test project directory
 pushd test2
 :: Make sure the test project can run
-./koka.exe -e bin/run.kk
+koka.exe -e "bin/run.kk"
